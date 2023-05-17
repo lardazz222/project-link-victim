5e16bccc2287b697872e4e58d2d0b6a6ba819a5c
lardazz222 <None>
2023-05-17 13:13:38 -0500

    Client script functionality resumed

--- client/src/client.py
+++ client/src/client.py
@@ -45,13 +45,12 @@ global_logger = Logger("Project-Link")
 global_logger.log("initiating services", "core")
 
 class RuntimeManager:
-
     def __init__(self):
         self.logger = Logger("Runtime")
         self.display = DisplayBuffer() # This is a service that will be used to capture the screen
         self.realtime_data = RealtimeDataService() # This is a service that provides organized data about the system
         self.job_handler = JobHandler() # This is a service that will handle jobs
-        api = SocketAPI(self.job_handler, host="127.0.0.1", port=5000)
+        # api = SocketAPI(self.job_handler, host="127.0.0.1", port=5000)
 
     def Start(self) -> None:
         """
@@ -60,10 +59,19 @@ class RuntimeManager:
         self._loop()
 
     def _loop(self):
-        self.logger.success("Started runtime loop", "threaded-service")
-        while True:
-            self.logger.log("Runtime tick 1000ms", "idle")
-            time.sleep(1)
+        try:
+            self.logger.success("Started runtime loop", "threaded-service")
+            while True:
+                # show latest frame
+                frame = self.display.get_latest_frame()
+                if frame is not None:
+                    frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+                    cv2.imshow('frame', frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+        except KeyboardInterrupt:
+            self.logger.log("Exiting Project-Link Client", "core")
+            sys.exit(0)
 
 if __name__ == "__main__":
     rt = RuntimeManager()
--- client/src/projectlink/DisplayBuffer.py
+++ client/src/projectlink/DisplayBuffer.py
@@ -9,47 +9,48 @@ class DisplayBuffer:
             DisplayBuffer.instance = DisplayBuffer.__DisplayBuffer(*args, **kwargs)
         return DisplayBuffer.instance
 
-    def __init__(self, monitor: int = 0, fps: int = 15) -> None:
-        self.monitor_id = monitor
-        self.fps = fps
-        self.camera = dxcam.create(monitor, 0, output_color="BGR")
-        self.camera.start(video_mode=True, target_fps=fps)
+    class __DisplayBuffer:
+        def __init__(self, monitor: int = 0, fps: int = 15) -> None:
+            self.monitor_id = monitor
+            self.fps = fps
+            self.camera = dxcam.create(monitor, 0, output_color="BGR")
+            self.camera.start(video_mode=True, target_fps=fps)
 
-        self._buffer = []
-        self.camera_thread = threading.Thread(target=self._update_thread)
-        self.camera_thread.daemon = True
+            self._buffer = []
+            self.camera_thread = threading.Thread(target=self._update_thread)
+            self.camera_thread.daemon = True
 
-        self.logger = Logger("DisplayBuffer")
+            self.logger = Logger("DisplayBuffer")
 
-        # start the thread
-        self.camera_thread.start()
-        self.running = True # This saves performance when the display buffer is not needed
+            # start the thread
+            self.camera_thread.start()
+            self.running = True # This saves performance when the display buffer is not needed
 
-    def get_latest_frame(self):
-        if len(self._buffer) == 0:
-            return None
-        return self._buffer[-1]
+        def get_latest_frame(self):
+            if len(self._buffer) == 0:
+                return None
+            return self._buffer[-1]
 
-    def get_latest_frame_and_compress(self, quality: int = 50):
-        if len(self._buffer) == 0:
-            return None
-        return cv2.imencode('.jpg', self._buffer[-1], [cv2.IMWRITE_JPEG_QUALITY, quality])[1].tobytes()
+        def get_latest_frame_and_compress(self, quality: int = 50):
+            if len(self._buffer) == 0:
+                return None
+            return cv2.imencode('.jpg', self._buffer[-1], [cv2.IMWRITE_JPEG_QUALITY, quality])[1].tobytes()
 
-    def _update_thread(self):
-        self.logger.success('started service: _update_thread', "threaded-service")
-        try:
-            while True:
-                if self.running:
-                    self._buffer.append(self.camera.get_latest_frame())
-                
-                time.sleep(1/self.fps)
-        except KeyboardInterrupt:
+        def _update_thread(self):
+            self.logger.success('started service: _update_thread', "threaded-service")
+            try:
+                while True:
+                    if self.running:
+                        self._buffer.append(self.camera.get_latest_frame())
+                    
+                    time.sleep(1/self.fps)
+            except KeyboardInterrupt:
+                self.camera.stop()
+                del self.camera
+                self.logger.log("exiting service: _update_thread", "threaded-service")
+
+        def SetMonitor(self, monitor: int):
+            self.monitor_id = monitor
             self.camera.stop()
-            del self.camera
-            self.logger.log("exiting service: _update_thread", "threaded-service")
-
-    def SetMonitor(self, monitor: int):
-        self.monitor_id = monitor
-        self.camera.stop()
-        self.camera = dxcam.create(monitor, 0, output_color="BGR")
-        self.camera.start(video_mode=True, target_fps=self.fps)
+            self.camera = dxcam.create(monitor, 0, output_color="BGR")
+            self.camera.start(video_mode=True, target_fps=self.fps)
